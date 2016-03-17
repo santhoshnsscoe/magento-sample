f75eb906d4755059732270e8891347eabab19d2d
Santhosh Kumar <santhoshnsscoe@gmail.com>
2016-03-17 11:15:57 +0530

    module code added

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# magento-sample
+# Magento 2 Sample Module
+
+This is a sample magento 2 module to explain on each concept that is explained in the blog http://mage-simplified.blogspot.in/
+
---
+++ registration.php
@@ -0,0 +1,8 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'VenderName_SampleModule',
+    __DIR__
+);
+
