00c2f5fe10ced6aa7c3b5b1a47c652d34e76646f
Santhosh Kumar <santhoshnsscoe@gmail.com>
2016-03-26 15:09:53 +0530

    attribute creation setup

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 
 This is a sample magento 2 module to explain on each concept that is explained in the blog http://mage-simplified.blogspot.in/search/label/magento2
 
-1. http://mage-simplified.blogspot.com/2016/02/magento-2-create-simple-module.html
-2. http://mage-simplified.blogspot.com/2016/02/magento-2-add-or-update-database.html
-3. http://mage-simplified.blogspot.com/2016/02/magento-2-events-and-observer.html
-4. http://mage-simplified.blogspot.com/2016/02/magento-2-theme-layout-and-templates.html
-5. http://mage-simplified.blogspot.com/2016/03/magento-2-add-to-admin-menu.html
-6. http://mage-simplified.blogspot.com/2016/03/magento-2-add-column-and-mass-action-to.html
-7. http://mage-simplified.blogspot.com/2016/03/magento-2-new-table-and-models.html
-8. http://mage-simplified.blogspot.com/2016/03/magento-2-add-admin-grid-page.html
+1. http://mage-simplified.blogspot.in/2016/02/magento-2-create-simple-module.html
+2. http://mage-simplified.blogspot.in/2016/02/magento-2-add-or-update-database.html
+3. http://mage-simplified.blogspot.in/2016/02/magento-2-events-and-observer.html
+4. http://mage-simplified.blogspot.in/2016/02/magento-2-theme-layout-and-templates.html
+5. http://mage-simplified.blogspot.in/2016/03/magento-2-add-to-admin-menu.html
+6. http://mage-simplified.blogspot.in/2016/03/magento-2-add-column-and-mass-action-to.html
+7. http://mage-simplified.blogspot.in/2016/03/magento-2-new-table-and-models.html
+8. http://mage-simplified.blogspot.in/2016/03/magento-2-add-admin-grid-page.html
 9. http://mage-simplified.blogspot.in/2016/03/magento-2-shell-script-or-commands.html
--- Setup/InstallData.php
+++ Setup/InstallData.php
@@ -5,11 +5,77 @@ namespace VendorName\SampleModule\Setup;
 use Magento\Framework\Setup\InstallDataInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Sales\Setup\SalesSetupFactory;
+use Magento\Quote\Setup\QuoteSetupFactory;
+use Magento\Catalog\Setup\CategorySetupFactory;
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
 
 class InstallData implements InstallDataInterface {
 
+    protected $_salesSetupFactory;
+    protected $_quoteSetupFactory;
+    protected $_categorySetupFactory;
+
+    public function __construct(SalesSetupFactory $salesSetupFactory, QuoteSetupFactory $quoteSetupFactory, CategorySetupFactory $categorySetupFactory) {
+        $this->_salesSetupFactory = $salesSetupFactory;
+        $this->_quoteSetupFactory = $quoteSetupFactory;
+        $this->_categorySetupFactory = $categorySetupFactory;
+    }
+
     public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context) {
-        //code to insert initial data to the database
+
+        $salesInstaller = $this->_salesSetupFactory->create(['resourceName' => 'sales_setup', 'setup' => $setup]);
+        $quoteInstaller = $this->_quoteSetupFactory->create(['resourceName' => 'quote_setup', 'setup' => $setup]);
+        $catalogInstaller = $this->_categorySetupFactory->create(['setup' => $setup]);
+
+        $this->addQuoteAttributes($quoteInstaller);
+        $this->addOrderAttributes($salesInstaller);
+        $this->addInvoiceAttributes($salesInstaller);
+        $this->addCreditmemoAttribute($salesInstaller);
+        $this->addCategoryAttributes($catalogInstaller);
+        $this->addProductAttributes($catalogInstaller);
+    }
+
+    public function addQuoteAttributes($installer) {
+        $installer->addAttribute('quote', 'sample_module_field', ['type' => 'text']);
+        $installer->addAttribute('quote_address', 'sample_module_field', ['type' => 'text']);
+    }
+
+    public function addOrderAttributes($installer) {
+        $installer->addAttribute('order', 'sample_module_field', ['type' => 'text']);
+    }
+
+    public function addInvoiceAttributes($installer) {
+        $installer->addAttribute('invoice', 'sample_module_field', ['type' => 'text']);
+    }
+
+    public function addCreditmemoAttribute($installer) {
+        $installer->addAttribute('creditmemo', 'sample_module_field', ['type' => 'text']);
+    }
+
+    public function addCategoryAttributes($installer) {
+        $installer->addAttribute('catalog_category', 'sample_module_field', [
+            'type' => 'varchar',
+            'label' => 'Sample Module Field',
+            'input' => 'text',
+            'required' => false,
+            'global' => ScopedAttributeInterface::SCOPE_STORE,
+            'group' => 'General Information',
+            'sort_order' => 30,
+                ]
+        );
+    }
+
+    public function addProductAttributes($installer) {
+        $installer->addAttribute('catalog_category', 'sample_module_field', [
+            'type' => 'varchar',
+            'label' => 'Sample Module Field',
+            'input' => 'text',
+            'global' => ScopedAttributeInterface::SCOPE_GLOBAL,
+            'user_defined' => true,
+            'required' => false,
+                ]
+        );
     }
 
 }
